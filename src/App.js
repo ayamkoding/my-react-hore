a79066d31589da498c96b1a8f560f7abd09159e8
ayamkoding <ayamkoding@gmail.com>
2017-04-01 06:10:55 +0700

    update file

--- src/App.js
+++ src/App.js
@@ -8,13 +8,14 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Selamat Datang di React</h2>
+          <h2>Selamat Datang di Aplikasi React</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
           <p className="App-asik">Percobaan break</p>
           <p>Content Inside</p>
+
       </div>
     );
   }
